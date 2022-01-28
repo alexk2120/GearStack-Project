659f9eab18a38219ef40b9da3328a3d318e2f21c
alexander.Khatamov <alexk2120@gmail.com>
2022-01-27 21:51:08 -0500

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,9 +11,26 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+         
+          <Route path="/signup">
+           {/* enter signup form here */}
+            <h1>Test Route</h1>
+          </Route>
+         
+          <Route path="/login">
+           {/* enter signup form here */}
+            <h1>Test Route</h1>
+          </Route>
+         
+          <Route path="/">
+            <h1>Page Count: {count}</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
